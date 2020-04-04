710f22eb79417c79622ea675842d38e6a2574003
Jenny Paduan <paje@mbari.org>
2020-04-04 00:29:10 +0000

    added urlpattern for mission.html...doesn't do what I want

--- mysite/mbdb/urls.py
+++ mysite/mbdb/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name='mbdb'
+
 urlpatterns = [
+    # ex: /mbdb/
     path('', views.index, name='index'),
-]
+    
+     # ex: /mbdb/5/
+    path('<int:expedition_id>/', views.detail, name='detail'),
+    path('<int:expedition_id>/detail/', views.detail, name='detail'),
+    path('<int:expedition_id>/mission/', views.mission, name='mission'),    
+    ]
+
